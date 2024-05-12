40815d7939b03fa020e65150e1e42ce0057962b2
Rayhaan1345 <96483570+Rayhaan1345@users.noreply.github.com>
2024-05-12 11:29:58 +0400

    Update website.py

--- website.py
+++ website.py
@@ -12,7 +12,7 @@ st.markdown("Hi!, Thank you for visiting my website! I'm a Research Intern at [B
 
 ##WORK
 st.header("🧪 My work: ")
-st.markdown("As mentioned previously, I'm a Research Intern at [Brown University](%s). The main goal of my internship is finding new methods for cancer removal without surgery, and how to stop cancer growth as early as possible. In addition, I am also undergoing advanced knowledge gaining through the Brown Pre-College Program. Apart from this, I am a sophomore at Podar International School (Cambridge), Nerul, Navi Mumbai CIE 9")
+st.markdown("As mentioned previously, I'm a Research Intern at [Brown University](%s). The main goal of my internship is finding new methods for cancer removal without surgery, and how to stop cancer growth as early as possible. In addition, I am also undergoing advanced knowledge gaining through the Brown Pre-College Program. Apart from this, I am a sophomore at Podar International School (Cambridge), Nerul, Navi Mumbai, India ")
 st.info('41/45 points 9th grade, Weighted: 9.7/10 GPA')
 #SKILL
 st.header("💼 My skills: ")
