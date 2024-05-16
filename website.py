aa949b49b66cafedf0a78bd7a21444ca539bfd08
Rayhaan Khan <rayhaankhan1345@gmail.com>
2024-05-16 15:21:23 +0400

    Update website.py

--- website.py
+++ website.py
@@ -6,16 +6,21 @@ components.iframe("https://media.licdn.com/dms/image/D4D03AQHW_lM3y3JYQA/profile
 
 ##ABOUT
 st.title("Rayhaan Khan")
-st.header(":violet[About Me: ]")
+st.header(":violet[About Me ]")
 brown = "https://www.brown.edu/"
 st.markdown("Hi!, Thank you for visiting my website! I'm a Research Intern at [Brown University](%s), Studying in Grade 10 in Podar International (CIE), Seawoods, Navi Mumbai, India. I love to delve deeper into concepts regarding Mathematics, AI, Deep Learning and especially Cancer Biology." % brown)
 
 ##WORK
-st.header("🧪 My work: ")
+st.header("🧪 My work ")
 st.markdown("As mentioned previously, I'm a Research Intern at [Brown University](%s). The main goal of my internship is finding new methods for cancer removal without surgery, and how to stop cancer growth as early as possible. In addition, I am also undergoing advanced knowledge gaining through the Brown Pre-College Program. Apart from this, I am a sophomore at Podar International School (Cambridge), Nerul, Navi Mumbai, India ")
 st.info(':red[**41/45 points 9th grade, Weighted: 9.7/10 GPA**]')
+#hobby
+st.header("📷 🦜My hobbies")
+st.markdown("I love wildlife (birds mainly) photography🦜, nature conservation 🌲❤️🐅")
+option = st.selectbox("Check out some of my work!")
+st.markdown("Along with 🏊🏻swimming, trying to achieve high typing speeds⌨️ and... RESEARCH!)
 #SKILL
-st.header("💼 My skills: ")
+st.header("💼 My proficiencies ")
 st.info("🐍 Python")
 st.info("VS Code")
 st.info("🔬 Research")
@@ -23,7 +28,7 @@ st.info("🧮 Advanced Mathematics")
 st.info("🖥️ 💻 Computer Hardware")
 
 ## Certifications
-st.header(" 🎓Certifications: " )
+st.header(" 🎓Certifications " )
 edx = 'https://courses.edx.org/certificates/b23e278c0b6d4735b83562769e9b1c71'
 st.markdown(":red[[HarvardX CS50's Introduction To Programming With Python (Verified edX)](%s)]" % edx)
 st.markdown("Fast.ai, Practical Deep Learning For Coders")
@@ -41,6 +46,8 @@ st.markdown("Visit: https://huggingface.co/spaces/RayhaanK/lungorbrain")
 
 st.subheader("Some Useful links: ")
 st.markdown("LinkedIn: https://www.linkedin.com/in/rrkhanrayhaan/")
+
+st.header("☎️ Ways to contact")
 me = st.checkbox("Email Adress: ")
 if me:
     st.info("rayhaankhan1345@gmail.com [preferred]")
