4d265c18442849286db2860e40653010b2d8627f
Rayhaan Khan <rayhaankhan1345@gmail.com>
2024-06-16 14:19:58 +0530

    Update website.py

--- website.py
+++ website.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+from streamlit_extras.badges import badge
 
 st.set_page_config(layout="centered", page_icon="pictureofme.png", page_title="Rayhaan Khan")
 
