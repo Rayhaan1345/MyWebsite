7fa5e0e6c56ec11b278427e3120fa97fa1370cb0
Rayhaan Khan <rayhaankhan1345@gmail.com>
2024-06-16 14:47:52 +0530

    Update website.py

--- website.py
+++ website.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="centered", page_icon="pictureofme.png", page_title="Rayhaan Khan")
 
 lnc = 'https://www.linkedin.com/in/rrkhanrayhaan/'
-st.markdown("[Linkedin (%s)]" % lnc")
+st.markdown("[Linkedin (%s)]" % lnc)
 
 components.iframe("https://media.licdn.com/dms/image/D4D03AQHW_lM3y3JYQA/profile-displayphoto-shrink_200_200/0/1712669123007?e=2147483647&v=beta&t=6-kK-ZzT_34_yoDvCQ_I-TSWpFUNvcCp-FgGM5NWepI", height=250)
 
