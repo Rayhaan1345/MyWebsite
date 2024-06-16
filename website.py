abe4508efb78af05b35899a1bf2baa6ac7f62f12
Rayhaan Khan <rayhaankhan1345@gmail.com>
2024-06-16 14:50:25 +0530

    Update website.py

--- website.py
+++ website.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="centered", page_icon="pictureofme.png", page_title="R
 components.iframe("https://media.licdn.com/dms/image/D4D03AQHW_lM3y3JYQA/profile-displayphoto-shrink_200_200/0/1712669123007?e=2147483647&v=beta&t=6-kK-ZzT_34_yoDvCQ_I-TSWpFUNvcCp-FgGM5NWepI", height=250)
 
 lnc = 'https://www.linkedin.com/in/rrkhanrayhaan/'
-st.markdown("[Linkedin] (%s)" % lnc)
+st.markdown("[Linkedin](%s)" % lnc)
 
 
 
